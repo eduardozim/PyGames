22d7c4f9fe1131a3e04883fe617d034e8f330bfa
Eduardo Zim <eduazim@gmail.com>
2020-12-08 11:01:33 -0300

    Version 0.1.2 - Refactor uodate screen

--- ALI3N_ATK/ALIENATTACK.py
+++ ALI3N_ATK/ALIENATTACK.py
@@ -6,7 +6,7 @@
 #
 # Released under the MIT License
 
-VERSION = "0.1"
+VERSION = "0.1.2"
 
 import sys
 import pygame
@@ -32,16 +32,24 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+            self._check_events()
+            self._update_screen()
+            
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()    
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
                 
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
-    
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+
+
 if __name__ == '__main__':
     # Make a game instance, and run the game.
     ai = AlienInvasion()
