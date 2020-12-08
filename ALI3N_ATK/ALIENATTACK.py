b4d5921637ad92661c78e6db751593460562a244
Eduardo Zim <eduazim@gmail.com>
2020-12-08 14:15:19 -0300

    Version 0.2 - Include ship Movement

--- ALI3N_ATK/ALIENATTACK.py
+++ ALI3N_ATK/ALIENATTACK.py
@@ -6,7 +6,7 @@
 #
 # Released under the MIT License
 
-VERSION = "0.1.2"
+VERSION = "0.2"
 
 import sys
 import pygame
@@ -33,6 +33,7 @@ class AlienInvasion:
         while True:
             # Watch for keyboard and mouse events.
             self._check_events()
+            self.ship.update()
             self._update_screen()
             
     def _check_events(self):
@@ -40,6 +41,16 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()    
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
